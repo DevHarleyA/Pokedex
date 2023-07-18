ea8bbf067480b31745074159dc2afc4eb47200a8
DevHarleyA <aharleydev@gmail.com>
2023-07-18 17:09:11 -0400

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Alexandra Harley on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
